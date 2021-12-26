838ab4053412c0f944a03f48ff748d42472d8482
shaula <shaulajasminetya22@gmail.com>
2021-12-26 11:40:18 +0700

    Fix 404 page not found and downgrade version React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import pages from './pages';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -9,10 +9,12 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" exact component={pages.LandingPage}></Route>
-        <Route path="/properties/:id" component={pages.DetailPage}></Route>
-        <Route path="/checkout" component={pages.Checkout}></Route>
-        <Route component={pages.PageNotFound} />
+        <Switch>
+          <Route path="/" exact component={pages.LandingPage}></Route>
+          <Route path="/properties/:id" component={pages.DetailPage}></Route>
+          <Route path="/checkout" component={pages.Checkout}></Route>
+          <Route path="" component={pages.PageNotFound} />
+        </Switch>
       </Router>
       <ToastContainer />
     </div>
